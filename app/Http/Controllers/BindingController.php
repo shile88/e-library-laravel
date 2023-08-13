25bd4b2d33e4355e9c91921a4769fe89640c5c82
Nikola Londrović <nikolalondrovic41@gmail.com>
2023-08-13 18:18:41 +0200

    Made the necessary models, controllers, requests, migrations, policies, factories and seeders

---
+++ app/Http/Controllers/BindingController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreBindingRequest;
+use App\Http\Requests\UpdateBindingRequest;
+use App\Models\Binding;
+
+class BindingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreBindingRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Binding $binding)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Binding $binding)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateBindingRequest $request, Binding $binding)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Binding $binding)
+    {
+        //
+    }
+}
