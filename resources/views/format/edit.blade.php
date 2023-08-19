b0f83010fbc42c86af14b7b76aef5061cb181e19
Matija Tijanic <matijatijanic@gmx.com>
2023-08-19 12:18:10 +0200

    Added variable for dynamic title in Genre and Format edit view file

--- resources/views/format/edit.blade.php
+++ resources/views/format/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Format')
+@section('title', $formatName)
 
 @section('content')
 
--- resources/views/genre/edit.blade.php
+++ resources/views/genre/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Žanr')
+@section('title', $genreName)
 
 @section('content')
 
