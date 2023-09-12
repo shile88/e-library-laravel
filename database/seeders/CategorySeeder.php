32e7329101d6b6a55f634490a50cbf00f014f1e6
Nikola Londrović <nikolalondrovic41@gmail.com>
2023-09-12 17:02:34 +0200

    Added description and icon to categories seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,16 +14,16 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            ['name' => 'Umjetnost i fotografija'],
-            ['name' => 'Biogrfija'],
-            ['name' => 'Dječije knjige'],
-            ['name' => 'Zanat i hobiji'],
-            ['name' => 'Krimi i triler'],
-            ['name' => 'Hrana i piće'],
-            ['name' => 'Grafički romani, Anime & Mange'],
-            ['name' => 'Istorija i arheologija'],
-            ['name' => 'Um, tijelo i duh'],
-            ['name' => 'Naučna fantastika, fikcija i horor']
+            ['name' => 'Umjetnost i fotografija', 'icon' => 'fas fa-palette', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Biogrfija', 'icon' => 'fas fa-address-book', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Dječije knjige', 'icon' => 'fas fa-children', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Zanat i hobiji', 'icon' => 'fas fa-screwdriver-wrench', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Krimi i triler', 'icon' => 'fas fa-gun', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Hrana i piće', 'icon' => 'fas fa-burger', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Grafički romani, Anime & Mange', 'icon' => 'fas fa-tv', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Istorija i arheologija', 'icon' => 'fas fa-calendar-day', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Um, tijelo i duh', 'icon' => 'fas fa-brain', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit'],
+            ['name' => 'Naučna fantastika, fikcija i horor', 'icon' => 'fas fa-rocket', 'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit']
         ]);
     }
 }
