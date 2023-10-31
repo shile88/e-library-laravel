776c55ce96fcc02374f412ec3af5aad0f04d12cc
Nikola Londrović <nikolalondrovic41@gmail.com>
2023-10-31 22:09:02 +0100

    Made Book show page

--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -1,7 +1,11 @@
 <?php
 
-function getPicturePath(string $url): string
+function getPicturePath($url)
 {
+    if(empty($url)) {
+        return App\Models\Book::DEFAULT_BOOK_PICTURE_PATH;
+    }
+
     // If url has "images" in it, it means that the picture is in different folder (in /public)
     if (str_contains($url, 'images')) {
         return $url;
--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,13 +1,102 @@
-@extends('layouts.app', [
-    'hasHeaderTitle' => true,
-])
+@extends('layouts.app')
 
 @section('title', $book->title)
 
 @section('content')
+    {{-- Heading --}}
+    <div class="heading">
+        <div class="flex justify-between border-b-[1px] border-[#e4dfdf]">
+            {{-- Main title --}}
+            <div class="pl-[30px] py-[10px] flex flex-col">
+                {{-- Name --}}
+                <h1>{{ $book->title }}</h1>
 
+                {{-- Breadcrumbs --}}
+                <div>
+                    <nav class="w-full rounded">
+                        <ol class="flex list-reset">
+                            <li>
+                                <a href="{{ route('books.index') }}" class="text-[#2196f3] hover:text-blue-600">
+                                    Books
+                                </a>
+                            </li>
+                            <li>
+                                <span class="mx-2">/</span>
+                            </li>
+                            <li>
+                                <a href="{{ route('books.show', $book) }}" class="text-[#2196f3] hover:text-blue-600">
+                                    {{ $book->title }}
+                                </a>
+                            </li>
+                        </ol>
+                    </nav>
+                </div>
+
+            </div>
+
+            {{-- Links and dropdown --}}
+            <div class="pt-[24px] mr-[30px]">
+                <a href="#" class="inline hover:text-blue-600">
+                    <i class="fas fa-level-up-alt mr-[3px]"></i>
+                    Otpisi knjigu
+                </a>
+                <a href="#" class="inline hover:text-blue-600 ml-[20px] pr-[10px]">
+                    <i class="far fa-hand-scissors mr-[3px]"></i>
+                    Izdaj knjigu
+                </a>
+                <a href="#" class="hover:text-blue-600 inline ml-[20px] pr-[10px]">
+                    <i class="fas fa-redo-alt mr-[3px] "></i>
+                    Vrati knjigu
+                </a>
+                <a href="#" class="hover:text-blue-600 inline ml-[20px] pr-[10px]">
+                    <i class="far fa-calendar-check mr-[3px] "></i>
+                    Rezervisi knjigu
+                </a>
+                <p
+                    class="inline cursor-pointer text-[25px] py-[10px] pl-[30px] border-l-[1px] border-[#e4dfdf] dotsKnjigaOsnovniDetalji hover:text-[#606FC7]">
+                    <i class="fas fa-ellipsis-v"></i>
+                </p>
+                <div class="z-10 hidden transition-all duration-300 origin-top-right transform scale-95 -translate-y-2 dropdown-knjiga-osnovni-detalji"
+                    style="display: none;">
+                    <div class="absolute right-0 w-56 mt-[7px] origin-top-right bg-white border border-gray-200 divide-y divide-gray-100 rounded-md shadow-lg outline-none"
+                        aria-labelledby="headlessui-menu-button-1" id="headlessui-menu-items-117" role="menu">
+                        <div class="py-1">
+                            <a href="{{ route('books.edit', $book) }}" tabindex="0"
+                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
+                                role="menuitem">
+                                <i class="fas fa-edit mr-[1px] ml-[5px] py-1"></i>
+                                <span class="px-4 py-0">Edit book</span>
+                            </a>
+                            <form action="{{ route('books.destroy', $book) }}" method="post">
+                                @csrf
+                                @method('delete')
+                                <p tabindex="0"
+                                    class="flex w-full px-4 text-sm leading-5 text-left
+                                             text-gray-700 hover:text-red-500 z-50">
+                                    <button type="submit" class="py-2 ml-[5px]" style="outline: none">
+                                        <i class="fa fa-trash mr-[1px]"></i>
+                                        <span class="px-4 py-0">Destroy book</span>
+                                    </button>
+                                </p>
+                            </form>
+                            {{-- <a href="#" tabindex="0"
+                                class="flex w-full px-4 py-2 text-sm leading-5 text-left text-gray-700 outline-none hover:text-blue-600"
+                                role="menuitem">
+                                <i class="fa fa-trash mr-[5px] ml-[5px] py-1"></i>
+                                <span class="px-4 py-0">Destroy book</span>
+                            </a> --}}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    {{-- Main content --}}
     <div class="pl-[30px] section- mt-[20px]">
         <div class="flex flex-row justify-between">
+            {{-- First column --}}
             <div class="mr-[30px]">
                 <div class="mt-[20px]">
                     <span class="text-gray-500 text-[14px]">Title</span>
@@ -47,6 +136,12 @@
                 </div>
             </div>
 
+            {{-- Second column --}}
+            <div class="mx-[30px]">
+                <img src="{{ getPicturePath($book->picture) }}" alt="Book picture" width="500">
+            </div>
+
+            {{-- Third column --}}
             <div class="mr-[70px] mt-[20px] flex flex-col max-w-[600px]">
                 <div>
                     <h4 class="text-gray-500 ">
@@ -63,7 +158,7 @@
                     <p class="font-medium">{{ $book->page_count }}</p>
                 </div>
                 <div class="mt-[20px]">
-                    <span class="text-gray-500 text-[14px]">Available copies</span>
+                    <span class="text-gray-500 text-[14px]">Number of copies</span>
                     <p class="font-medium">{{ $book->total_count }}</p>
                 </div>
                 <div class="mt-[20px]">
